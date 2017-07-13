4b2aa555ee87cd13df41f2f11a001defffa824de
Khang Huynh <khanghuynh92@gmail.com>
2017-07-13 22:20:32 +0700

    feat(Close #11): implement mock mode

--- controllers/Scraper.js
+++ controllers/Scraper.js
@@ -1,6 +1,14 @@
 import MiddelwaresWrapper from '../helpers/RouteMiddlewaresWrapper';
 import * as Scraper from './ScraperService';
 
-export const fetch = MiddelwaresWrapper(Scraper.fetch);
+export const fetch = process.env.NODE_ENV === 'mock' ? (req, res) => {
+  res.json({
+    message: 'Done',
+  });
+} : MiddelwaresWrapper(Scraper.fetch);
 
-export const healthCheck = MiddelwaresWrapper(Scraper.healthCheck);
+export const healthCheck = process.env.NODE_ENV === 'mock' ? (req, res) => {
+  res.json({
+    message: 'Done',
+  });
+} : MiddelwaresWrapper(Scraper.healthCheck);
