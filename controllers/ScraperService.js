7a17351008376eda20a20a6d850f6c50edcec132
Khang Huynh <khanghuynh92@gmail.com>
2017-11-04 14:59:44 +0700

    fix(Close #34): Validate fetch request’s host if it’s reader admin

--- controllers/ScraperService.js
+++ controllers/ScraperService.js
@@ -1,14 +1,23 @@
+import { URL } from 'url';
+import { BAD_REQUEST } from 'http-status';
 import fetchData from '../helpers/Fetch';
 import crawl from './../helpers/Crawl';
+import APIError from '../helpers/APIError';
 
-const fetch = async (req, res) => {
+const adminUrl = new URL(process.env.SCRAPER_ADMIN_URL);
+
+const fetch = (req, res, next) => {
+  if (adminUrl.host !== req.headers.host) {
+    return next(new APIError('Scraper admin url invalid', BAD_REQUEST, true));
+  }
+
   if (process.env.SCRAPER_TYPE === 'api') {
     fetchData();
   } else {
     crawl();
   }
 
-  res.json({
+  return res.json({
     message: 'Done',
   });
 };
